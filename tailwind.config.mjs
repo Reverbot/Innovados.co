7145828bade09b307777d65b1961fd84ebfba615
Donald Schiffmann <schiffmann10@gmail.com>
2024-10-12 18:47:39 -0400

    style update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        pulseCircle: "pulse-animation 1s",
+      },
+      keyframes: {
+        "pulse-animation": {
+          "0%": {
+            boxShadow: "0 0 0 0px #3b82f6",
+          },
+          "100%": {
+            boxShadow: "0 0 0 20px rgba(0, 0, 0, 0)",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
